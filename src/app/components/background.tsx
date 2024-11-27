961535ff5e17c7a521c7653257f18036f753e68d
LeoEstevesProgramando <148232222+LeoFEsteves@users.noreply.github.com>
2024-11-27 09:05:25 -0300

    Delete src/app directory

--- src/app/components/background.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-
-export default function Background({children}: {children: ReactNode}){
-
-    return(
-        <div className="flex w-full bg-gray-black text-white">{children}</div>
-    )
-}
--- src/app/components/header.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-
-export default function Header({children}: {children: ReactNode}){
-
-    return(
-        <header className="flex w-full bg-dark-black text-white">{children}</header>
-    )
-}
